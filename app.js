15597ad59f07eb57ceee37f9c16474306471907c
NartakarW <jc.sanchez1616@hotmail.com>
2020-03-01 22:41:47 -0500

    front

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//sass --watch scss/index.scss:public/stylesheets/style.css
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
