ddf742eefd2c954b6f1914bcffd269fd808cdc40
NartakarW <jc.sanchez1616@hotmail.com>
2020-03-02 16:19:04 -0500

    biblioteca

--- public/js/biblioteca.js
+++ public/js/biblioteca.js
@@ -16,8 +16,8 @@ const changeTab = (tab) => {
     lista.forEach(ele => {
         resultado += `
             <div class="col-xl-4 col-md-6 col-12">
-                <div class="card items-lista" onclick="buscarEjercicios('${ele}')">
-                    <h3 class=" centrado-v">${ele}</h3>
+                <div class="card items-lista" style='height:150px' onclick="buscarEjercicios('${ele}')">
+                    <h3 class=" centrado-v" >${ele}</h3>
                 </div>
             </div>`;
     })
@@ -28,19 +28,52 @@ const changeTab = (tab) => {
 const buscarEjercicios = (categoria) => {
     let tabN = document.querySelector('#bibliotecaTabs .nav-link.active').name;
 
-    let route = `/${tabN}/get/?muscle:''`;
+    let route = `/${tabN}/get/?focus:''`;
     let list = document.querySelector('.biblioteca-list');
 
     fetch(route).then(res => res.json()).then((lista) => {
         let resultado = '';
         list.innerHTML = '';
         lista.forEach(ele => {
-            resultado += `
+            if (tabN == 'exercises')
+                resultado += `
             <div class="col-xl-4 col-md-6 col-12">
                 <div class="card items-lista">
-                    <h3 class=" centrado-v">${ele}</h3>
+                    <h3 >${ele.name}</h3>
+                    <h5>${ele.muscle} nivel: ${ele.difficulty} </h5>
+                    <p> ${ele.description}</p>
+                    <a href='${ele.link}'> Video</a>
+                    <p> Hiciste este ejercicio hoy?</p>
+                    <svg viewBox="0 0 80 80" class="ap-4"  style=" fill:#F5DE93">
+                         <use xlink:href="/icons/add.svg#add"></use>
+                     </svg>
                 </div>
             </div>`;
+            else if (tabN == 'routines') {
+                resultado += `<div class="col-xl-4 col-md-6 col-12">
+                    <div class="card items-lista">
+                        <h3 >${ele.name}</h3>
+                        <h5>${ele.focus}  </h5>
+                    <p> Hiciste esta rutina hoy?</p>
+                        <svg viewBox="0 0 80 80" class="ap-4"  style=" fill:#F5DE93">
+                         <use xlink:href="/icons/add.svg#add"></use>
+                     </svg>
+                    </div>
+                </div>`;
+            }
+            else {
+                resultado += `<div class="col-xl-4 col-md-6 col-12">
+                    <div class="card items-lista">
+                        <h3 >${ele.name}</h3>
+                        <h5>${ele.focus} nivel: ${ele.difficulty}</h5>
+                        <h5> ${ele.routines.size} rutinas </h5>
+                    <p> Hiciste este entrenamiento hoy?</p>
+                        <svg viewBox="0 0 80 80" class="ap-4"  style=" fill:#F5DE93">
+                         <use xlink:href="/icons/add.svg#add"></use>
+                     </svg>
+                    </div>
+                </div>`;
+            }
         })
         list.innerHTML = resultado;
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,26 @@
 
+const getHistory = (day) => {
+    document.querySelector('.number.selected').classList.toggle('selected');
+    day.classList.toggle('selected');
+    resum.innerHTML = '';
+    let load = document.createElement("li");
+    load.classList.add('list-group-item')
+    load.textContent = 'loading';
+    resum.append(load);
+    fetch(`/historicals/get/?user=userId&date=${day.textContent}-2-2020`).then(res => res.json()).then((list) => {
 
-const getHistory = (day)=>{
-        document.querySelector('.number.selected').classList.toggle('selected');
-        day.classList.toggle('selected');
-        resum.innerHTML='';
-         let load = document.createElement("li");
-        load.classList.add('list-group-item')
-        load.textContent= 'loading';
-        resum.append(load);
-    fetch('/exercises/list').then(res=>res.json()).then((list)=>{
-        
-        resum.innerHTML='';
+        resum.innerHTML = '';
         list.forEach(element => {
-        let display = document.createElement("li");
-        display.classList.add('list-group-item')
-        display.textContent= element.name;
-        resum.append(display)
+            let display = document.createElement("li");
+            display.classList.add('list-group-item')
+            display.textContent = element.name;
+            resum.append(display)
         });
     })
-    
+
 }
 
 const resum = document.getElementById('infoHistory');
 
-const months=['Enero', 'Febrero','Marzo','Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre', 'Noviembre', 'Diciembre']
-const espaciados=[2,5,6,2,4,0,2,5,1,3,6,1];
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const espaciados = [2, 5, 6, 2, 4, 0, 2, 5, 1, 3, 6, 1];
