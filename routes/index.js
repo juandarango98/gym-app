3784b4424c6f9b824baaf57904bbc78062f8a229
NartakarW <jc.sanchez1616@hotmail.com>
2020-03-02 07:31:23 -0500

    load parcial

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('login', { title: 'Express' });
+  res.render('index', { title: 'Express', page: 'login' });
 });
 
+router.get('/profile', (req,res)=>{
+  res.render('index', { title: 'Express', page: 'profile', month:'July' })
+})
 module.exports = router;
